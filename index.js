10c3c9f8eac59a600703fd4eb8f6e8f191f2d85a
oscar3488 <e3488176@gmail.com>
2022-07-27 01:16:48 +0800

    add

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+
+function Serch(){
+    let search_content = document.getElementById("n. vmp6").value;
+    window.open(`https://www.google.com.tw/search?q=${search_content}` );
+    // alert(search_content)
+    // console.log(search_content)
+}
+
